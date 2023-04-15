9f9fbee8ac7627f4bf650e1df11f34d064152b41
soyeonvv <sovvworld@gmail.com>
2023-04-15 14:39:34 +0900

    상품 목록 - 타임리프

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,12 +10,11 @@ public class Item {
     private Integer price;
     private Integer quantity;
 
-    public Item(String itemName, Integer price, Integer quantity) {
+    public Item() {
 
     }
 
-    public Item(Long id, String itemName, Integer price, Integer quantity) {
-        this.id = id;
+    public Item(String itemName, Integer price, Integer quantity) {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
